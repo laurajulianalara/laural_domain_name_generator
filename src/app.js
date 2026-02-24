a4196b62b80f7ee0ccf18167e991c4c43fe80423
Laura Lara <laurajulianabusiness@gmail.com>
2026-02-24 16:46:15 +0000

    pushing final code

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let htmlElement = this.document.querySelector(".domain-name-list");
+
+
+  const pronoun = ["my", "your", "our", "his" , "her"];
+  const adjective = ["great", "divine", "exquisite", "exciting"];
+  const noun = ["dog", "horse", "elephant", "lawyer", "programmer", "doctor"];
+  const extension = [".com", ".net", ".io", ".dev", ".us", ".cn", ".es", ".org", ".me", ".law"];
+
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
+    for (let adjectiveIndex = 0; adjectiveIndex < adjective.length; adjectiveIndex ++) {
+      for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
+        for (let extensionIndex = 0; extensionIndex < extension.length; extensionIndex++) {
+          const newDiv = document.createElement("div");
+          newDiv.textContent = pronoun[pronounIndex] + adjective[adjectiveIndex] + noun[nounIndex] + extension[extensionIndex];
+          htmlElement.appendChild(newDiv);
+          console.log(pronoun[pronounIndex] + adjective[adjectiveIndex] + noun[nounIndex] + extension[extensionIndex]);
+
+
+
+
+
+
+        }
+      }
+    }
+  }
 };
